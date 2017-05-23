e3168c5f0535b0a3666d58e5be042a59c17235e7
Gordana <minjon56@gmail.com>
2017-05-23 17:36:43 +0200

    Added font awesome

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,53 +1,55 @@
-
 @extends('layouts.admin')
-@section('title', ' | All Cars')
-          @section('content')
-          @include('flash::message')
-            <h1 class="page-header" style="padding-top:40px;">All Cars</h1>
 
-          
-          <div class="table-responsive">
-            <table class="table table-striped" id="myTable" class="tablesorter">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Name</th>
-                  <th>Model</th>
-                  <th>Displacement  {{ config('app.displacement') }}</th>
-                  <th>Status</th>
-                  <th>Date added</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                @php
-                  $i = 1
-                @endphp
-                @foreach ($cars as $car)
-                  <tr>
-                  <td>{{ $i++ }}</td>
-                  <td><a href="{{ $car->path('show') }}">{{ $car->name }}</a></td>
-                  <td>{{ $car->model }}</td>
-                  <td>{{ $car->displacement }}</td>
-                  <td>{{ $car->category->name }}</td>
-                  <td>{{ $car->created_at }}</td>
-                  <td>
-                    <a href="{{ $car->path('edit') }}" class="btn btn-warning">Edit car details</a>
-                    
-                  </td>
-                  
-                </tr>
-                @endforeach
-              </tbody>
-            </table>
-          </div>
-          
-        </div>
-      </div>
-    </div>
-  <div class="text-center">{{ $cars->links() }}</div>
+@section('title', ' | All Cars')
+
+@section('content')
+
+@include('flash::message')
+
+<h1 class="page-header" style="padding-top:40px;">All Cars</h1>
+
+<div class="table-responsive">
+  <table class="table table-striped" id="myTable" class="tablesorter">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Model</th>
+        <th>Displacement  {{ config('app.displacement') }}</th>
+        <th>Status</th>
+        <th>Created</th>
+        <th><i class="fa fa-cog" aria-hidden="true"></i></th>
+      </tr>
+    </thead>
+    <tbody>
+      @php
+      $i = 1
+      @endphp
+      @foreach ($cars as $car)
+      <tr>
+        <td>{{ $i++ }}</td>
+        <td><a href="{{ $car->path('show') }}">{{ $car->name }}</a></td>
+        <td>{{ $car->model }}</td>
+        <td>{{ $car->displacement }}</td>
+        <td>{{ $car->category->name }}</td>
+        <td>{{ $car->created_at }}</td>
+        <td>
+          <a href="{{ $car->path('edit') }}" class="btn btn-warning">Edit car details</a>
+
+        </td>
+
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+</div>
+
+</div>
+</div>
+</div>
+<div class="text-center">{{ $cars->links() }}</div>
 @endsection
-    
-    
-  </body>
+
+
+</body>
 </html>
